11a3758d1af1c09157387eac19d3f2a7daf96325
RyanMrts <ryanmartinssilva2006@gmail.com>
2023-03-24 22:23:24 -0300

    exercicio pronto

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿string frasedigitada, frasecensurada;
+Console.Write("escreva um discurso de ódio: ");
+
+frasedigitada = Console.ReadLine()!;
+ frasecensurada = frasedigitada
+
+.Replace("chato", "*#@*!$")
+.Replace("chata", "*#@*!$")
+.Replace("boba", "*#@*!$")
+.Replace("feio", "*#@*!$")
+.Replace("feia", "*#@*!$")
+.Replace("boboca", "*#@*!$")
+.Replace("bocó", "*#@*!$")
+.Replace("tonto", "*#@*!$")
+.Replace("tonta", "*#@*!$")
+.Replace("palerma", "*#@*!$")
+.Replace("paspalho", "*#@*!$")
+.Replace("paspalha", "*#@*!$")
+.Replace("tantã", "*#@*!$")
+.Replace("panaca", "*#@*!$")
+.Replace("pentelho", "*#@*!$")
+.Replace("pentelha", "*#@*!$")
+.Replace("burro", "*#@*!$")
+.Replace("burra", "*#@*!$")
+.Replace("besta", "*#@*!$");
+
+Console.WriteLine(frasecensurada );
